d22d7d17af7de071b1a2d9ba38e91bcb75a23fdf
byonghun <byonghunkwak@gmail.com>
2019-01-14 17:34:59 -0800

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,73 @@
 /** @format */
 import {Navigation} from 'react-native-navigation';
 import {registerComponents} from './src/config/register';
+import {width} from './src/helpers/dimensions';
+
+const bottomTabs = {
+  children: [
+    {
+      component: {
+        name: 'gp.about',
+        options: {
+          bottomTab: {
+            text: 'About'
+          }
+        }
+      }
+    },
+    {
+      component: {
+        name: 'gp.sermons',
+        options: {
+          bottomTab: {
+            text: 'Sermons'
+          }
+        }
+      }
+    },
+    {
+      component: {
+        name: 'gp.service',
+        options: {
+          bottomTab: {
+            text: 'Service'
+          }
+        }
+      }
+    },
+    {
+      component: {
+        name: 'gp.bible',
+        options: {
+          bottomTab: {
+            text: 'Bible'
+          }
+        }
+      }
+    }
+  ]
+}
+
+const sideMenu = {
+  left: {
+    component: {
+      name: 'gp.prayer'
+    }
+  },
+  center: {
+    stack: {
+      id: "CenterStack",
+      children: [{
+        bottomTabs
+      }]
+    }
+  },
+  right: {
+    component: {
+      name: 'gp.profile'
+    }
+  }
+}
 
 registerComponents()
 
@@ -8,54 +75,19 @@ Navigation.events().registerAppLaunchedListener(() => {
   Navigation.setDefaultOptions({
     bottomTabs: {
       backgroundColor: 'pink'
+    },
+    sideMenu: {
+      right: {
+        width: width * .85
+      },
+      left: {
+        width: width * .85
+      }
     }
   })
   Navigation.setRoot({
     root: {
-      bottomTabs: {
-        children: [
-          {
-            component: {
-              name: 'gp.about',
-              options: {
-                bottomTab: {
-                  text: 'About'
-                }
-              }
-            }
-          },
-          {
-            component: {
-              name: 'gp.sermons',
-              options: {
-                bottomTab: {
-                  text: 'Sermons'
-                }
-              }
-            }
-          },
-          {
-            component: {
-              name: 'gp.service',
-              options: {
-                bottomTab: {
-                  text: 'Service'
-                }
-              }
-            }
-          },
-          {
-            component: {
-              name: 'gp.bible',
-              options: {
-                bottomTab: {
-                  text: 'Bible'
-                }
-              }
-            }
-          }
-        ]
-      }
+      sideMenu
     }
   })
 });
