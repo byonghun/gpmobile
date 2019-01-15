0c88110321174c04daf50b8602750bf3521bd9fc
byonghun <byonghunkwak@gmail.com>
2019-01-14 17:35:30 -0800

    Update register.js file

--- src/config/register.js
+++ src/config/register.js
@@ -3,12 +3,18 @@ import {
   about,
   bible,
   sermons,
-  service
+  service,
+  prayer,
+  profile,
+  sermonAudio
 } from './components.json';
 import About from '../components/Tabs/About';
 import Bible from '../components/Tabs/Bible';
 import Sermons from '../components/Tabs/Sermons';
 import Service from '../components/Tabs/Service';
+import Profile from '../components/Drawers/Profile';
+import Prayer from '../components/Drawers/Prayer';
+import SermonAudio from '../components/Stacks/Audio/sermon';
 
 const registerComponent = (name, Component) =>
   Navigation.registerComponent(name, () => Component)
@@ -18,4 +24,7 @@ export const registerComponents = () => {
   registerComponent(bible, Bible);
   registerComponent(sermons, Sermons);
   registerComponent(service, Service);
+  registerComponent(profile, Profile);
+  registerComponent(prayer, Prayer);
+  registerComponent(sermonAudio, SermonAudio);
 }
